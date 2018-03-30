488b778ca379909a02142bd1167e30d78e5a1fbf
Amanda M <amandaseagull3@gmail.com>
2018-03-30 14:12:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
+katz_deli = [] 
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.empty?
+ puts "The line is currently empty."
+ else
+   "The line is currently: #{other_deli[0]} #{other_deli[0]} #{other_deli_number[1]} #{other_deli[1]} #{other_deli_number[2]} #{other_deli[2]}"
+ end
+ puts 
+end
+
+def take_a_number(katz_deli, name)
+  puts #{name} #{index}
+end
+
+def now_serving()
+  puts "Currently serving #{name}."
+  
+  puts "There is nobody waiting to be served!"
+end
