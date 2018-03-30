4965568de61a96aacf3aa452bf3300fa972d1e7a
Amanda M <amandaseagull3@gmail.com>
2018-03-30 14:56:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 katz_deli = [] 
 other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 # Write your code here.
 
 def line(katz_deli)
