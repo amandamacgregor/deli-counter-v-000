14b59cb07255d183853113990add923c18376ecf
Amanda M <amandaseagull3@gmail.com>
2018-03-30 14:36:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   if deli.empty?
  puts "The line is currently empty."
  else
-   "The line is currently: #{other_deli[0]} #{other_deli[0]} #{other_deli_number[1]} #{other_deli[1]} #{other_deli_number[2]} #{other_deli[2]}"
+   line = "The line is currently: #{other_deli[0]} #{other_deli[0]} #{other_deli_number[1]} #{other_deli[1]} #{other_deli_number[2]} #{other_deli[2]}"
  end
  puts 
 end
