cf0e7a5cd0b08043bacca16d664876168ef6c4d5
Amanda M <amandaseagull3@gmail.com>
2018-03-30 14:59:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,10 +11,22 @@ def line(deli)
  else
    line = "The line is currently:"
    deli.each.with_index(1) do |name, index|
-     return line << " #{index}. #{name}"
+     line << " #{index}. #{name}"
  end
  puts line
  end
+end
+
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
 end
 
 def take_a_number(katz_deli, name)
